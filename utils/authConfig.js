fa0f35b4308490abb1aaeeff3ed4222e169b7c34
me <amanvolly@gmail.com>
2021-03-16 13:33:38 +1300

    update config, came with boilerplate that was pointing to http not s

--- utils/authConfig.js
+++ utils/authConfig.js
@@ -2,7 +2,7 @@ const authConfig = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRETSTRING,
-  baseURL: 'http://trackgroceries.info',
+  baseURL: 'https://trackgroceries.info',
   clientID: process.env.AUTH0CLIENTID,
   issuerBaseURL: process.env.BASEURL 
 };
